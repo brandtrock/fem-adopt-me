59e23034840af892dbda0eb65b4e91cc371f70a4
Brandt Jorgensen <brandt.jorgensen@datasite.com>
2024-09-04 16:17:35 -0500

    JSX and eslint config

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,15 @@
-import React from "react";
 import { createRoot } from "react-dom";
-
-const Pet = (props) => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, props.name),
-    React.createElement("h2", {}, props.animal),
-    React.createElement("h2", {}, props.breed),
-  ]);
-};
+import Pet from "./Pet";
 
 const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Adopt Me!"),
-    React.createElement(Pet, {
-      animal: "Dog",
-      name: "Sophie",
-      breed: "Minature Goldendoodle",
-    }),
-    React.createElement(Pet, {
-      animal: "Bird",
-      name: "Pepper",
-      breed: "Cockatiel",
-    }),
-    React.createElement(Pet, {
-      animal: "Cat",
-      name: "Doink",
-      breed: "Mixed",
-    }),
-  ]);
+  <div>
+    <h1>Adopt Me!</h1>
+    <Pet name="Sophie" animal="Dog" breed="Mini Goldendoodle" />
+    <Pet name="Pepper" animal="Bird" breed="Cockatiel" />
+    <Pet name="Doink" animal="Cat" breed="Mixed" />
+  </div>;
 };
 
 const container = document.getElementById("root");
 const root = createRoot(container);
-root.render(React.createElement(App));
+root.render(<App />);
